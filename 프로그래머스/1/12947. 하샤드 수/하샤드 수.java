c4c7bdeafb123f3e818933223b3fae9665ac6729
wltjdqo55 <132214230+wltjdqo55@users.noreply.github.com>
2024-06-03 17:57:41 +0900

    [level 1] Title: 하샤드 수, Time: 5.24 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String s = x + "";
+        int sum = 0;
+        for(int i=0;i<s.length();i++){
+            sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+        
+        return x % sum == 0 ? true : false;
+    }
+}
