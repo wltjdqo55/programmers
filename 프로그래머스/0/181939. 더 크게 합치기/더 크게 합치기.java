1642077daf781853d484ccbe7c6386079d98adfd
wltjdqo55 <132214230+wltjdqo55@users.noreply.github.com>
2024-06-03 17:47:44 +0900

    [level 0] Title: 더 크게 합치기, Time: 10.57 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        return  Math.max(Integer.parseInt(""+a+b), Integer.parseInt(""+b+a));
+        
+        
+    }
+}
