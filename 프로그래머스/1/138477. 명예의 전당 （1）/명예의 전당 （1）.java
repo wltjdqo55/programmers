f0497b349c02bd7b003f53eeceb007f8c53d879d
wltjdqo55 <132214230+wltjdqo55@users.noreply.github.com>
2024-06-03 17:50:36 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.31 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> list = new ArrayList<>(k); // 명예의 전당
+       
+        for(int i=0;i<score.length;i++){
+
+            list.add(score[i]);
+            list.sort(Comparator.naturalOrder());
+            if(list.size()>k){
+                list.remove(0);
+            }
+            answer[i] = list.get(0);
+        }
+
+        
+        return answer;
+    }
+}
