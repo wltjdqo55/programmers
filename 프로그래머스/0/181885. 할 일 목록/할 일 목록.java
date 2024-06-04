13d66f544f4f2cb8a460531d62fd2b95834cfe1e
wltjdqo55 <132214230+wltjdqo55@users.noreply.github.com>
2024-06-04 16:11:48 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
+        
+        ArrayList<String> list = new ArrayList();        
+        for(int i=0;i<todo_list.length;i++){
+            if(!finished[i]){
+                list.add(todo_list[i]);
+            }
+        }
+        
+        
+        return list;
+    }
+}
