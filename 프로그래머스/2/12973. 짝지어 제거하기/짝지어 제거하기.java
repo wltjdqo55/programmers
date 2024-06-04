f241a1a1bf8bd3411edbf3d009008af7fcd88864
wltjdqo55 <132214230+wltjdqo55@users.noreply.github.com>
2024-06-04 09:35:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.11 ms, Memory: 58.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.isEmpty()) { 
+                stack.push(s.charAt(i));
+            } else {
+                char ch = s.charAt(i);
+                if (stack.peek() == ch) {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
