ef36725bb2fd7abf03a7798a4ed41300825840b5
Seong-bae Ji <132214230+wltjdqo55@users.noreply.github.com>
2024-06-10 10:42:22 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        
+        if(angle == 180) return 4;
+        else if(angle > 0 && angle <90) return 1;
+        else if(angle == 90) return 2;
+        else return 3;
+       
+    }
+}
