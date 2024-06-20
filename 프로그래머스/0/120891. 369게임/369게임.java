6a7d3ad39a83138c80815fa0ca349d5d0cc48db6
Seong-bae Ji <132214230+wltjdqo55@users.noreply.github.com>
2024-06-20 09:52:34 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order > 0){
+            if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9) answer += 1;
+            order /= 10;
+        }
+        return answer;
+    }
+}
