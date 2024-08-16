62e4df97cf44814099ecadb9cc26b19cef8086e2
Seong-bae Ji <132214230+wltjdqo55@users.noreply.github.com>
2024-08-16 10:51:56 +0900

    [level 0] Title: A 강조하기, Time: 0.10 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        myString = myString.toLowerCase();
+        
+        answer = myString.replaceAll("a", "A");
+        
+        return answer;
+    }
+}
