455ea850a699a71eeecd76f6d949b70fc5a71c6e
Seong-bae Ji <132214230+wltjdqo55@users.noreply.github.com>
2024-08-21 17:10:14 +0900

    [level 0] Title: n개 간격의 원소들, Time: 6.27 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        
+        ArrayList<Integer> list = new ArrayList<>();
+         
+        for(int i=0;i<num_list.length;i++){
+            if(i%n==0){
+                list.add(num_list[i]);
+            }
+        }
+        
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
