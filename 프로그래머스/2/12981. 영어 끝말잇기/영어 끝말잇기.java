e41346074ce092d8c5a001979514aed45abeccfc
Seong-bae Ji <132214230+wltjdqo55@users.noreply.github.com>
2024-08-28 16:27:45 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        
+        HashMap<String, Boolean> map = new HashMap<>();
+        
+        char lastChar = words[0].charAt(words[0].length() - 1);
+        
+        for(int i = 0 ; i < words.length ; i ++) {
+            String word = words[i];
+            int player = (i % n) + 1;
+            int turn = (i / n) + 1;
+            
+            if(map.containsKey(word)) {
+                answer[0] = player;
+                answer[1] = turn;
+                return answer;
+            }
+            
+            if(i > 0 && word.charAt(0) != lastChar) {
+                answer[0] = player;
+                answer[1] = turn;
+                return answer;
+            }
+            
+            map.put(word, true);
+            lastChar = word.charAt(word.length() - 1);
+        }
+       
+        return answer;
+    }
+}
